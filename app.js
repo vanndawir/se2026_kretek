02b48d1227ca2f24b5da1ccbc8a5504444d35ec7
vanndawir <vanndawir@gmail.com>
2026-08-12 08:10:32 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -431,6 +431,8 @@ app.get('/', async (req, res) => {
     });
 });
 
-server.listen(3000, () => {
-    console.log('🚀 Server berjalan sukses di http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+    console.log(`🚀 Server berjalan sukses di http://localhost:${PORT}`);
 });
